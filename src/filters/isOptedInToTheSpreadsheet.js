67c378f652896358020483d39471e08300cbed9a
Ladybug <auburnete@gmail.com>
2019-08-31 18:37:08 +1000

    whoops

--- src/filters/isOptedInToTheSpreadsheet.js
+++ src/filters/isOptedInToTheSpreadsheet.js
@@ -2,7 +2,7 @@
 const blends = require('../gcp/blends.js');
 
 
-module.exports = async (filterDirection) => {
+module.exports = (filterDirection) => {
     let final = async (message) => {
         let value = await blends.getProperty(message.author.id, 'rdzip_consent');
         return value === filterDirection;

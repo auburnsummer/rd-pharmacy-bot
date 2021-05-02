23e4f3dff5fd4b1d81fa32b78afe4ec7a74a8fe2
huantianad <davidtianli@gmail.com>
2021-05-01 22:32:12 -0700

    improve message, fix typos

--- src/actions/rdzip_wupsrdlevel.js
+++ src/actions/rdzip_wupsrdlevel.js
@@ -6,5 +6,5 @@ const config = require('./config.js');
 
 module.exports = async (message, results) => {
         // Perhaps inprove on this message with an image + better instructions? mostly just a placeholder for now ig.
-        return message.author.send(`It looks like you've sent a rdlevel file in <#${config.SHOWCASE_CHANNEL}>; .rdlevel files do not external files such as the song file or any other assets you may have used. It's highly recommened that you first export your level to a .rdzip in the editor.`);
+        return message.author.send(`It looks like you've sent a rdlevel file in <#${config.SHOWCASE_CHANNEL}>; .rdlevel files do not contain external files such as the song file or any other assets you may have used, so the level may not work properly. Please publish your level as an .rdzip file so people can play it as you intended.`);
 }
